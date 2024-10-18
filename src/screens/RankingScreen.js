6305266e5450f67e274965ea9cc5c61108c037e4
Andres Calderon <acaldup@gmail.com>
2024-10-17 21:00:44 -0500

    Finish module ranking

--- src/screens/RankingScreen.js
+++ src/screens/RankingScreen.js
@@ -1,9 +1,26 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { useEffect, useState } from "react";
+import { collection, limit, onSnapshot, orderBy, query } from "firebase/firestore";
+import { db } from "../utils";
+import { RestaurantRaking } from "../components/Restaurants";
 
 const RankingScreen = () => {
+  const [restaurants, setRestaurants] = useState(null);
+
+  useEffect(() => {
+    const q = query(collection(db, "restaurants"), orderBy("ratingMedia", "desc"), limit(4));
+    onSnapshot(q, snapshot => {
+      const array = [];
+      for (const doc of snapshot.docs) {
+        array.push(doc.data());
+      }
+      setRestaurants(array);
+    });
+  }, []);
+
   return (
     <View>
-      <Text>RankingScreen</Text>
+      <RestaurantRaking restaurants={restaurants} />
     </View>
   );
 };
